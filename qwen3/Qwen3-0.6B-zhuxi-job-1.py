2896f34e655de91122960ffceb46279696e1b9b7
triptips-top <triptips.top@gmail.com>
2026-02-11 15:33:09 +0800

    1

--- qwen3/Qwen3-0.6B-zhuxi-job-1.py
+++ qwen3/Qwen3-0.6B-zhuxi-job-1.py
@@ -1,7 +1,17 @@
-from trl import SFTTrainer
+from trl import SFTTrainer, SFTConfig
 from datasets import load_dataset
+from peft import LoraConfig
 
 trainer = SFTTrainer(
     model=r"C:\Users\John\.cache\huggingface\hub\models--Qwen--Qwen3-0.6B\snapshots\c1899de289a04d12100db370d81485cdf75e47ca",
-    train_dataset=load_dataset("json", data_files="Qwen3-0.6B-zhuxi-job-1.json", split="train"))
+    train_dataset=load_dataset("json",
+                               data_files="Qwen3-0.6B-zhuxi-job-1.json",
+                               split="train"),
+    args=SFTConfig(learning_rate=2.0e-4),
+    peft_config=LoraConfig(r=32,
+                           lora_alpha=16,
+                           lora_dropout=0.05,
+                           bias="none",
+                           task_type="CAUSAL_LM")
+)
 trainer.train()
