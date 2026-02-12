8a9cd3e0f07810909e4167dab9806b7b0493cd0f
triptips-top <triptips.top@gmail.com>
2026-02-13 00:12:43 +0800

    1

---
+++ gemma3/gemma-3-270m-it-adapter.py
@@ -0,0 +1,12 @@
+from peft import PeftModel
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+model_path = r"C:\Users\John\.cache\huggingface\hub\models--google--gemma-3-270m-it\snapshots\ac82b4e820549b854eebf28ce6dedaf9fdfa17b3"
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+model = AutoModelForCausalLM.from_pretrained(model_path)
+
+model = PeftModel.from_pretrained(
+    model, "trainer_output/gemma-3-270m-it-zhuxi-job")
+
+model = model.merge_and_unload()
+model.save_pretrained("trainer_output/gemma-3-270m-it-zhuxi-job")
