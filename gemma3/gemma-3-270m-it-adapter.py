67299d6a8aec0f11722cff7415f2e64ac027a9eb
triptips-top <triptips.top@gmail.com>
2026-02-14 13:29:33 +0800

    1

--- gemma3/gemma-3-270m-it-adapter.py
+++ gemma3/gemma-3-270m-it-adapter.py
@@ -6,7 +6,7 @@ tokenizer = AutoTokenizer.from_pretrained(model_path)
 model = AutoModelForCausalLM.from_pretrained(model_path)
 
 model = PeftModel.from_pretrained(
-    model, "trainer_output/gemma-3-270m-it-zhuxi-job")
+    model, "trainer_output/checkpoint-30")
 
 model = model.merge_and_unload()
-model.save_pretrained("trainer_output/gemma-3-270m-it-zhuxi-job")
+model.save_pretrained("trainer_output/checkpoint-30")
--- qwen3/Qwen3-0.6B-adapter.py
+++ qwen3/Qwen3-0.6B-adapter.py
@@ -6,7 +6,7 @@ tokenizer = AutoTokenizer.from_pretrained(model_path)
 model = AutoModelForCausalLM.from_pretrained(model_path)
 
 model = PeftModel.from_pretrained(
-    model, "trainer_output/Qwen3-0.6B-zhuxi-job")
+    model, "trainer_output/checkpoint-30")
 
 model = model.merge_and_unload()
-model.save_pretrained("trainer_output/Qwen3-0.6B-zhuxi-job")
+model.save_pretrained("trainer_output/checkpoint-30")
